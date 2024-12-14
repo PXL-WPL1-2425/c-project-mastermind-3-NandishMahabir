8514e3d262628f309d1ec32f8983c9a8565fb327
Nandish Mahabir <nandish.mahabir@student.pxl.be>
2024-12-14 19:43:02 +0100

    Mastermind-12, Mastermind-13, Mastermind-14

---
+++ Mastermind/Data.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace Mastermind
+{
+    public static class Data
+    {
+        private static Random random = new Random();
+        private static DispatcherTimer timer = new DispatcherTimer();
+        private static int secondsPassed;
+        public static int HighscoresSaved { get; set; }
+        public static int Attempts { get; set; }
+        public static int PossibleAttempts { get; set; }
+        public static int AmountOfPlayers { get; set; }
+        public static int CurrentPlayer { get; set; }
+        public static List<string> PlayerNames { get; set; }
+        public static int Score { get; set; }
+        static Dictionary<SolidColorBrush, string> colors = new Dictionary<SolidColorBrush, string>();
+        public static Dictionary<SolidColorBrush, string> Colors
+        {
+            get { return colors; }
+        }
+        private static List<string> colorCode = new List<string>();
+        public static List<string> ColorCode
+        {
+            get {  return colorCode; }
+        }
+        private static string[] highscores;
+
+        public static string[] Highscores
+        {
+            get { return highscores; }
+            set { highscores = value; }
+        }
+
+        public static bool Color1LabelAdded { get; set; }
+        public static bool Color2LabelAdded { get; set; }
+        public static bool Color3LabelAdded { get; set; }
+        public static bool Color4LabelAdded { get; set; }
+        static Data()
+        {
+            colors.Add(Brushes.Red, "Red");
+            colors.Add(Brushes.Orange, "Orange");
+            colors.Add(Brushes.Yellow, "Yellow");
+            colors.Add(Brushes.Green, "Green");
+            colors.Add(Brushes.Blue, "Blue");
+            colors.Add(Brushes.White, "White");
+            Attempts = 1;
+            HighscoresSaved = 0;
+            Score = 100;
+            PossibleAttempts = 10;
+            highscores = new string[15]; 
+            PlayerNames = new List<string>();
+            timer.Tick += new EventHandler(CheckTimer);
+            timer.Interval = new TimeSpan(0, 0, 1);
+        }
+        public static void GenerateRandomColorCode()
+        {
+            colorCode.Clear();
+            for (int i = 0; i < 4; i++)
+            {
+                int index = random.Next(0, colors.Count);
+                colorCode.Add(colors.ElementAt(index).Value);
+            }
+        }
+        public static List<int> ValidateColorCode(List<string> inputColors)
+        {
+            List<int> points = new List<int>();
+            for(int i = 0; i < inputColors.Count; i++)
+            {
+                if (inputColors[i].Equals(ColorCode[i]))
+                {
+                    points.Add(2);
+                }
+                else if (ColorCode.Contains(inputColors[i]))
+                {
+                    points.Add(1);
+                    Score -= 1;
+                }
+                else
+                {
+                    points.Add(0);
+                    Score -= 2;
+
+                }
+            }
+            ResetBooleans();
+            return points;
+        }
+        public static void ResetBooleans()
+        {
+            Color1LabelAdded = false;
+            Color2LabelAdded = false;
+            Color3LabelAdded = false;
+            Color4LabelAdded = false;
+        }
+
+        public static void IncreaseAttempst()
+        {
+            Attempts++;
+        }
+        /// <summary>
+        /// De ToogleDebug methode zorgt er voor dat de debug
+        /// mode in- of uitgeschakeld wordt.
+        /// </summary>
+        /// <param name="textBox">Naam van de Textbox die verborgen/weergegeven wordt door de debug methode.</param>
+        public static void ToggleDebug(TextBox textBox)
+        {
+            if (textBox.Text == string.Empty)
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < colorCode.Count; i++)
+                {
+                    stringBuilder.Append($"{colorCode[i]} ");
+                }
+                textBox.Text = stringBuilder.ToString();
+            }
+            if (textBox.Visibility == System.Windows.Visibility.Visible)
+            {
+                textBox.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            else
+            {
+                textBox.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+        /// <summary>
+        /// De StartCountdown functie start een timer met een
+        /// interval van 1 seconde.
+        /// </summary>
+        public static void StartCountdown()
+        {
+            secondsPassed = 0;
+            timer.Start();
+        }
+        /// <summary>
+        /// De StopCountdown methode laat de timer na 10 seconden stoppen.
+        /// </summary>
+        public static void CheckTimer(object sender, EventArgs e)
+        {
+            secondsPassed++;
+            if (secondsPassed == 10)
+            {
+                StopCountdown();
+                IncreaseAttempst();
+                Score -= 8;
+            }
+        }
+        public static void StopCountdown()
+        {
+            timer.Stop();
+            secondsPassed = 0;
+        }
+        public static bool CheckGameOver()
+        {
+            if (Attempts > PossibleAttempts - 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public static void ResetGame()
+        {
+            Score = 100;
+            secondsPassed = 0;
+            Attempts = 1;
+            StopCountdown();
+
+            GenerateRandomColorCode();
+        }
+    }
+}
---
+++ Mastermind/MainWindow.xaml.cs
@@ -0,0 +1,330 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Mastermind
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer titleUpdate = new DispatcherTimer();
+        List<StackPanel> stackPanels = new List<StackPanel>();
+        int timesvalidated;
+        public MainWindow()
+        {
+            InitializeComponent();
+            StartGame();
+            Data.GenerateRandomColorCode();
+            FillComboBoxes();
+
+            titleUpdate.Tick += new EventHandler(Update);
+            titleUpdate.Interval = new TimeSpan(0, 0, 1);
+            titleUpdate.Start();
+
+            stackPanels.Add(StackColor1);
+            stackPanels.Add(StackColor2);
+            stackPanels.Add(StackColor3);
+            stackPanels.Add(StackColor4);
+            timesvalidated = 0;
+        }
+        public void Update(Object sender, EventArgs e)
+        {
+            this.Title = $"Poging {Data.Attempts}";
+            LblAttempts.Content = $"Attempt: {Data.Attempts}";
+            LblPlayerName.Content = $"Player: {Data.PlayerNames[Data.CurrentPlayer]}";
+            LblScore.Content = $"Score: {Data.Score}";
+            if (Data.Attempts - timesvalidated > 1)
+            {
+                foreach(StackPanel stack in stackPanels)
+                {
+                    if(stack.Children.Count - timesvalidated > 0)
+                    {
+                        stack.Children.RemoveAt(timesvalidated);
+                    }
+
+                    Label label = new Label();
+                    label.Height = 32;
+                    label.Width = 32;
+                    label.Margin = new Thickness(5, 0, 5, 0);
+                    label.Background = Brushes.DarkSlateGray;
+
+                    stack.Children.Add(label);
+                }
+                ClearComboBoxSelection();
+                Data.ResetBooleans();
+                timesvalidated++;
+            }
+        }
+        private void FillComboBoxes()
+        {
+            foreach(var color in Data.Colors)
+            {
+                CboColor1.Items.Add(color.Value);
+                CboColor2.Items.Add(color.Value);
+                CboColor3.Items.Add(color.Value);
+                CboColor4.Items.Add(color.Value);
+            }
+        }
+        private void CboColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cbo = sender as ComboBox;
+            if(cbo.SelectedIndex != -1)
+            {
+                if (cbo.Name.Equals("CboColor1"))
+                {
+                    AddOrChangeLabel(StackColor1, cbo, Data.Color1LabelAdded);
+                    Data.Color1LabelAdded = true;
+                }
+                else if (cbo.Name.Equals("CboColor2"))
+                {
+                    AddOrChangeLabel(StackColor2, cbo, Data.Color2LabelAdded);
+                    Data.Color2LabelAdded = true;
+                }
+                else if (cbo.Name.Equals("CboColor3"))
+                {
+                    AddOrChangeLabel(StackColor3, cbo, Data.Color3LabelAdded);
+                    Data.Color3LabelAdded = true;
+                }
+                else if (cbo.Name.Equals("CboColor4"))
+                {
+                    AddOrChangeLabel(StackColor4, cbo, Data.Color4LabelAdded);
+                    Data.Color4LabelAdded = true;
+                }
+            }
+        }
+        private void AddOrChangeLabel(StackPanel stackPanel,ComboBox comboBox, bool labelAdded)
+        {
+            if (!labelAdded)
+            {
+                Label label = new Label();
+                label.Height = 32;
+                label.Width = 32;
+                label.Margin = new Thickness(5, 0, 5, 0);
+
+                label.Background = GetColorBrush(comboBox);
+
+                stackPanel.Children.Add(label);
+            }
+            else
+            {
+                Label label = (Label)stackPanel.Children[stackPanel.Children.Count - 1];
+                label.Background = GetColorBrush(comboBox);
+            }
+
+        }
+        private SolidColorBrush GetColorBrush(ComboBox comboBox)
+        {
+            SolidColorBrush brush = Data.Colors.Where(x => x.Value.Equals(comboBox.SelectedValue))
+                                   .Select(x => x.Key).First();
+            return brush;
+        }
+        private void BtnValidateCode_Click(object sender, RoutedEventArgs e)
+        {
+            if(CboColor1.SelectedIndex != -1 && CboColor2.SelectedIndex != -1 && CboColor3.SelectedIndex != -1 && CboColor4.SelectedIndex != -1)
+            {
+                timesvalidated++;
+                Data.StopCountdown();
+                List<int> points = Data.ValidateColorCode(new List<string> { CboColor1.SelectedValue.ToString(), CboColor2.SelectedValue.ToString(), CboColor3.SelectedValue.ToString(), CboColor4.SelectedValue.ToString() });
+                List<StackPanel> stackPanels = new List<StackPanel>() { StackColor1, StackColor2, StackColor3, StackColor4 };
+                for (int i = 0; i < stackPanels.Count; i++)
+                {
+                    ChangeBorder(stackPanels[i], points[i]);
+                }
+                if (!Data.CheckGameOver())
+                {
+                    int score = 0;
+                    foreach (int point in points)
+                    {
+                        score += point;
+                    }
+                    if (score == 8)
+                    {
+                        if (Data.CurrentPlayer < Data.PlayerNames.Count - 1)
+                        {
+                            MessageBox.Show($"You Managed to crack the right code,your score is {Data.Score} points! \nNow It's {Data.PlayerNames[Data.CurrentPlayer + 1]} his/her turn", Data.PlayerNames[Data.CurrentPlayer]);
+                            Data.Highscores.SetValue($"{Data.PlayerNames[Data.CurrentPlayer]} - {Data.Attempts} pogingen - {Data.Score}/100", Data.HighscoresSaved);
+                            Data.HighscoresSaved++;
+                            Data.CurrentPlayer++;
+                            ClearUI();
+                            Data.ResetGame();
+                            Data.StopCountdown();
+                            Data.StartCountdown();
+                        }
+                        else
+                        {
+                            MessageBox.Show($"You Managed to crack the right code,your score is {Data.Score} points!", Data.PlayerNames[Data.CurrentPlayer]);
+                            Data.Highscores.SetValue($"{Data.PlayerNames[Data.CurrentPlayer]} - {Data.Attempts} pogingen - {Data.Score}/100", Data.HighscoresSaved);
+                            Data.HighscoresSaved++;
+                        }
+                    }
+                    else
+                    {
+                        Data.StartCountdown();
+                    }
+                }
+                else
+                {
+                    if (Data.CurrentPlayer < Data.PlayerNames.Count - 1)
+                    {
+                        MessageBox.Show($"You did not manage to crack the right code, the correct code was {string.Join(" ", Data.ColorCode)}. Better luck next time! \nNow It's {Data.PlayerNames[Data.CurrentPlayer + 1]} his/her turn", Data.PlayerNames[Data.CurrentPlayer]);
+                        Data.CurrentPlayer++;
+                        ClearUI();
+                        Data.ResetGame();
+                        Data.StopCountdown();
+                        Data.StartCountdown();
+                    }
+                    else
+                    {
+                        MessageBox.Show($"You did not manage to crack the right code, the correct code was {string.Join(" ", Data.ColorCode)}. Better luck next time!", Data.PlayerNames[Data.CurrentPlayer]);
+                    }
+                        
+                }
+                ClearComboBoxSelection();
+                Data.IncreaseAttempst();
+
+            }
+            else
+            {
+                MessageBox.Show("Make sure to select a color at all positions.", "Error");
+            }
+        }
+        private void ChangeBorder(StackPanel stackPanel, int points)
+        {
+            if (points == 2)
+            {
+                Label label = stackPanel.Children[stackPanel.Children.Count - 1] as Label;
+                label.BorderBrush = Brushes.DarkRed;
+                label.BorderThickness = new Thickness(3);
+            }
+            else if (points == 1)
+            {
+                Label label = stackPanel.Children[stackPanel.Children.Count - 1] as Label;
+                label.BorderBrush = Brushes.Wheat;
+                label.BorderThickness = new Thickness(3);
+            }
+        }
+        private void ClearComboBoxSelection()
+        {
+            CboColor1.SelectedIndex = -1;
+            CboColor2.SelectedIndex = -1;
+            CboColor3.SelectedIndex = -1;
+            CboColor4.SelectedIndex = -1;
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F12 && e.KeyboardDevice.Modifiers == ModifierKeys.Control)
+            {
+                Data.ToggleDebug(ColorCodeTextbox);
+            }
+        }
+
+        private void ClearUI()
+        {
+            foreach (StackPanel stack in stackPanels)
+            {
+                stack.Children.Clear();
+            }
+            ColorCodeTextbox.Clear();
+            timesvalidated = 0;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Do you really want to close the game?", "Are you sure?", MessageBoxButton.YesNo);
+            if(result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void MnuAfsluiten_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MnuNieuwSpel_Click(object sender, RoutedEventArgs e)
+        {
+            ClearUI();
+            Data.ResetGame();
+            StartGame();
+        }
+
+        private void MnuHighscores_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string score in Data.Highscores)
+            {
+                if (!(score is null))
+                {
+                    sb.AppendLine(score);
+                }
+            }
+            MessageBox.Show(sb.ToString(), "Mastemind highscores");
+        }
+
+        private void MnuAantalPogingen_Click(object sender, RoutedEventArgs e)
+        {
+            string input  = Interaction.InputBox("How many attempts would you like to have?", "Amount of attempts", "10", 500);
+            int pogingen = 0;
+            while (string.IsNullOrEmpty(input) || !int.TryParse(input, out pogingen) || pogingen > 20 || pogingen < 3)
+            {
+                MessageBox.Show("Please select a number between 3 and 20!", "Invalid number");
+                input = Interaction.InputBox("How many attempts would you like to have?", "Amount of attempts", "10", 500);
+            }
+            Data.PossibleAttempts = pogingen;
+            ClearUI();
+            Data.ResetGame();
+            Data.StartCountdown();
+        }
+
+        private void StartGame()
+        {
+            List<string> names = new List<string>();
+            bool addAnotherPlayer = true;
+
+            names.Add(AskName());
+            while (addAnotherPlayer)
+            {
+                MessageBoxResult result = MessageBox.Show("Would you like to add another player?", "Select Players", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    names.Add(AskName());
+                }
+                else
+                {
+                    addAnotherPlayer = false;
+                }
+            }
+            Data.PlayerNames = names;
+            Data.AmountOfPlayers = names.Count;
+            Data.CurrentPlayer = 0;
+            Data.StartCountdown();
+        }
+
+        private string AskName()
+        {
+            string playerName = Interaction.InputBox("What is your name?", "Player Name", "", 500);
+            while (string.IsNullOrEmpty(playerName))
+            {
+                MessageBox.Show("Please select a valid name!", "Invalid Name");
+                playerName = Interaction.InputBox("What is your name?", "Player Name", "", 500);
+            }
+            return playerName;
+        }
+    }
+}
